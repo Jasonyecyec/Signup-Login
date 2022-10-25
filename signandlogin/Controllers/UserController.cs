6044d090efb8dfa2678ee65c8aed1995f5470e77
Jasonyecyec <jasonyecyec@gmail.com>
2022-10-25 17:01:57 +0800

    hashed password, display user photo

--- signandlogin/Controllers/UserController.cs
+++ signandlogin/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using signandlogin.Models;
 using signandlogin.DBModel;
 using Newtonsoft.Json;
 using System.Net.Mail;
+using System.Web.Helpers;
 
 namespace signandlogin.Controllers
 {
@@ -14,7 +15,7 @@ namespace signandlogin.Controllers
     {
 
 
-        // GET: User
+        //========== SIGN UP==================
         public ActionResult SignUp()
         {
             UserModel user = new  UserModel();
@@ -24,30 +25,40 @@ namespace signandlogin.Controllers
         [HttpPost]
         public ActionResult SignUp(UserModel userObject)
         {
-
+            
             if (ModelState.IsValid)
             {
+                //create object of database
                 using (users_dbEntities userDBEntities = new users_dbEntities())
                 {
+                    //check if email doesnt  exist in users table
                     if (!userDBEntities.users.Any(m => m.email == userObject.email))
                     {
+                        //Generate 6 random digit for OTP
                         Random r = new Random();
                         int randNum = r.Next(1000000);
                         string userOtp = randNum.ToString("D6");
 
+                        //hash password
+                        string salt = Crypto.GenerateSalt();
+                        string password = userObject.password + salt;
+                        string hashedPass = Crypto.HashPassword(password);
+
+                        //create object of userTable, then pass the UserModel object data
                         user userTable = new DBModel.user();
                         userTable.first_name = userObject.firstName;
                         userTable.last_name = userObject.lastName;
                         userTable.email = userObject.email;
-                        userTable.password = userObject.password;
+                        userTable.password = hashedPass;
+                        userTable.password_salt = salt;
                         userTable.address = userObject.address;
                         userTable.gender = userObject.gender;
                         userTable.phone_number = userObject.phoneNumber;
                         userTable.birthdate = userObject.birthdate;
                         userTable.date_created = DateTime.Now;
-                        userTable.otp = userOtp;
+                        userTable.user_photo = "~/Images/User_Photo/default_user_photo.jpg";
 
-                 
+                        //create session for OTP, store email and userTable in tempdata
                         Session["otp"] = userOtp;
                         TempData["email"] = userObject.email;
                         TempData["userTable"] = JsonConvert.SerializeObject(userTable);
@@ -65,12 +76,12 @@ namespace signandlogin.Controllers
             return View();
         }
 
+        //========== EMAIL VALIDATION ==============================
         public ActionResult EmailValidation()
         {
-      
             using (MailMessage mail = new MailMessage())
                 {
-                  
+                   // send the otp to email
                     mail.To.Add(TempData["email"].ToString());
                     mail.From = new MailAddress("jason.yecyec023@gmail.com");
                     mail.Subject = "OTP";
@@ -92,22 +103,24 @@ namespace signandlogin.Controllers
         [HttpPost]
         public ActionResult EmailValidation(string otp)
         {
-          
-         
+          //check if the input OTP is same with OPT in session
             if (otp == Session["otp"].ToString())
             {
+                //call saveuserdatabase method
                 return RedirectToAction("SaveUserToDatabase");
             }
             else
             {
+                //return to page, and render invalid OTP
                 ModelState.AddModelError("Error", "Invalid OTP");
                 return View();
-            }
-
-      
+            }     
         }
+
+        //============= SAVING USER TO DATABASE ==================
         public ActionResult SaveUserToDatabase()
-        {
+        {   
+            //pass the userTable tempdata to new userTable object, then save to DB
             user userTable = JsonConvert.DeserializeObject<user>(TempData["userTable"].ToString());
             using (users_dbEntities userDBEntities = new users_dbEntities())
             {
@@ -115,12 +128,16 @@ namespace signandlogin.Controllers
                 userDBEntities.SaveChanges();
             }
 
+            //remove OTP session
+            Session.Remove("otp");
+            //create name session and display in Homepage
             Session["name"] = userTable.first_name + " " + userTable.last_name;
+            Session["userPhoto"] = Url.Content(userTable.user_photo.Trim());
             return RedirectToAction("Index", "Home");
   
         }
 
-
+        //============== LOGIN ==========================
         public ActionResult LogIn()
         {
             return View();
@@ -133,16 +150,35 @@ namespace signandlogin.Controllers
             {
                 using (users_dbEntities userDBEntities = new users_dbEntities())
                 {
-                    var findUser = userDBEntities.users.Where(m => m.email == loginObject.email && m.password == loginObject.password).FirstOrDefault();
+                    //check if user email if exist in DB              
+                    var findUser = userDBEntities.users.Where(m => m.email == loginObject.email).FirstOrDefault();
+            
                    if (findUser != null)
                     {
+                        string inputPassword = loginObject.password + findUser.password_salt;
+                        var hashedPassword = findUser.password;
+                        var verifyPassword = Crypto.VerifyHashedPassword(hashedPassword, inputPassword);
 
-                        Session["name"] = findUser.first_name + " " + findUser.last_name;
-                        return RedirectToAction("Index", "Home");
+                        if (verifyPassword)
+                        {
+                            //get name and store in session, redirect to homepage
+                            Session["name"] = findUser.first_name + " " + findUser.last_name;
+                            Session["userPhoto"] = Url.Content(findUser.user_photo.Trim());
+
+                            return RedirectToAction("Index", "Home");
+                        }
+                        else
+                        {
+                            //if error, render errorm message
+                            ModelState.AddModelError("Error", "Wrong password");
+                            return View();
+                        }
+                  
                     }
                     else
                     {
-                        ModelState.AddModelError("Error", "Email and password do not match");
+                        //if error, render errorm message
+                        ModelState.AddModelError("Error", "Email doesn't exist");
                         return View();
                     }
                 }
@@ -150,11 +186,15 @@ namespace signandlogin.Controllers
             return View();
         }
 
+        //============== LOGOUT ==========================
         public ActionResult Logout()
         {
             Session.Abandon();
             return RedirectToAction("Index","Home");
         }
+
+
+
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
